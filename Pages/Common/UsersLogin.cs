14d82489643761d81a3758f0bb079d62488335f5
Smittal02022021 <113420178+Smittal02022021@users.noreply.github.com>
2022-12-26 18:37:36 +0530

    Update UsersLogin.cs
    Dummy changes

--- Pages/Common/UsersLogin.cs
+++ Pages/Common/UsersLogin.cs
@@ -6,6 +6,7 @@ namespace SalesForce_Project.Pages.Common
 {
     class UsersLogin : BaseClass
     {
+    //dummy changes
         By linkSetUp = By.CssSelector("a[id='setupLink']");
         By linkManageUsers = By.CssSelector("[id='Users_font']");
         By linkUsers = By.Id("ManageUsers_font");
